0fa76e420a55cd6e9befded46eab546ea9cc54ca
Shinji13 <keskasaymen8@gmail.com>
2024-05-22 20:21:43 +0100

    completing ui for author page

--- src/routes/(internal)/reading/authors/[authorId]/+page.svelte
+++ src/routes/(internal)/reading/authors/[authorId]/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import Avatar from '$components/other/avatar.svelte';
+	import WritingBackground from '$components/other/background.svelte';
 	import type { Contribution } from '$global/types.global';
 	import type { User } from 'lucia';
 
-	export let data: { info: User; work: Contribution[] };
+	export let data: { info: User; contributions: Contribution[] };
 </script>
 
 <div class="author">
@@ -11,7 +12,7 @@
 		<Avatar avatar={data.info.avatar} />
 		<div class="identity">
 			<h4>{data.info.username}</h4>
-			<span>{data.info.id}</span>
+			<span>ID:{data.info.id}</span>
 		</div>
 	</section>
 	<section class="about">
@@ -20,6 +21,20 @@
 	</section>
 	<section class="work">
 		<h3>Work</h3>
+		<div class="contributions">
+			{#each data.contributions as contribution}
+				<a href="/reading/{contribution.writingId}" class="contribution">
+					<div>
+						<h4>{contribution.writingName}</h4>
+						<WritingBackground background={contribution.writingBackground} />
+					</div>
+					<div>
+						<span> <span class="context">Writing Time</span> is {contribution.writingTime}</span>
+						<span><span class="context">Role</span> is {contribution.role}</span>
+					</div>
+				</a>
+			{/each}
+		</div>
 	</section>
 </div>
 
@@ -38,33 +53,79 @@
 		color: var(--primaryColor);
 	}
 
-	.bio {
+	.bio,
+	.about,
+	.work {
+		width: 100%;
 		display: flex;
 		flex-direction: column;
-		gap: 1rem;
+		gap: 0.5rem;
+	}
+
+	.bio {
+		align-items: center;
 		--width: 8rem;
 		--color: var(--primaryColor);
 	}
 
-	.identity {
+	.bio .identity {
 		display: flex;
 		align-items: center;
 		gap: 0.5rem;
 	}
-	.identity :is(h4, span) {
+
+	.bio .identity :is(h4, span) {
 		color: var(--foregroundColor);
 	}
-	.about {
-		display: flex;
-		flex-direction: column;
-	}
+
 	.about p {
 		color: var(--foregroundColor);
+	}
+
+	.contributions {
+		width: 100%;
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 1rem;
+	}
+
+	.contribution {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		padding: 0.5rem;
+		border-radius: var(--border-radius);
+		background-color: color-mix(in srgb, var(--foregroundColor) 8%, transparent 92%);
+		border: 2px solid var(--foregroundColor);
+	}
+
+	.contribution:hover {
+		border-color: var(--primaryColor);
+		box-shadow:
+			2px 2px 2px var(--primaryColor),
+			2px 2px 2px var(--primaryColor),
+			2px 2px 2px var(--primaryColor);
+	}
+
+	.contribution div {
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		color: var(--foregroundColor);
+	}
+
+	.contribution .context {
+		color: var(--primaryColor);
 	}
 
 	@media screen and (width < 764px) {
 		.bio {
 			--width: 5rem;
+		}
+		.contributions {
+			grid-template-columns: 100%;
 		}
 	}
 </style>
